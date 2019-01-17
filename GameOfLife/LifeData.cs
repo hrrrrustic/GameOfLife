9f76a890a4ffbfd96848dc057163b02818491286
Hrrrrustic <hrrrrustic@gmail.com>
2019-01-17 05:42:01 +0300

    <some shitcode>

--- GameOfLife/LifeData.cs
+++ GameOfLife/LifeData.cs
@@ -1,28 +1,117 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace GameOfLife
 {
     public class LifeData
     {
-        public bool[,] States = new bool[20, 20];
+        private bool[,] States = new bool[20, 20];
+        private bool[,] NextStates = new bool[20, 20];
+        private bool[] Values = new bool[400];
         public LifeData()
         {
-            
+                
         }
 
         public void MakeTurn()
         {
-            //TODO: implement logic
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    int aliveCount = GetInfo(i, j);
+                    GetNextPositions(i, j, aliveCount);
+                }
+            }
+            
         }
 
         public void PaintButtons(Button[,] buttons)
         {
-            //TODO: implement logic
+            int k = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    States[i, j] = NextStates[i, j];
+                    Values[k++] = States[i, j];
+                    buttons[i, j].Background = States[i, j] == true ? Brushes.Black : Brushes.White;
+                }
+            }
         }
 
-        public void ReverseState(int x, int y)
+        public void ReverseState(Button thisButton, int y, int x)
         {
-            //TODO: implement logic
+            thisButton.Background = thisButton.Background == Brushes.Black ? Brushes.White : Brushes.Black;
+            States[y, x] = thisButton.Background == Brushes.Black ? true : false;
+        }
+        private int GetInfo(int y, int x)
+        {
+            int aliveCount = 0;
+            for (int i = y - 1; i < y + 2; i++)
+            {
+                for (int j = x - 1; j < x + 2; j++)
+                {
+                    if (i == y && j == x)
+                        continue;
+                    int posXCopy = j == -1 ? 19 : j == 20 ? 0 : j;
+                    int posYCopy = i == -1 ? 19 : i == 20 ? 0 : i;
+                    if (States[posYCopy, posXCopy])
+                        aliveCount++;
+                }
+            }
+            return aliveCount;
+        }
+        private void GetNextPositions(int y, int x, int aliveCount)
+        {
+            if (States[y, x])
+            {
+                if (aliveCount > 3 || aliveCount < 2)
+                    NextStates[y, x] = false;
+                else
+                    NextStates[y, x] = true;
+            }
+            else
+            {
+                if (aliveCount == 3)
+                    NextStates[y, x] = true;
+                else
+                    NextStates[y, x] = false;
+            }
+        }
+        public void SaveInBMP()
+        {
+            int height = 20; //Пока что фиксированное значение
+            int width = 20; //Тоже самое
+            int stride = width * 4;
+            byte[] bits = new byte[height * stride];
+            for (int i = 0; i < bits.Length - 3; i += 4) 
+            {
+                if (States[(i / 4) / 20, (i / 4) % 20]) 
+                {
+                    bits[i] = 0;
+                    bits[i + 1] = 0;
+                    bits[i + 2] = 0;
+                }
+                else
+                {
+                    bits[i] = 255;
+                    bits[i + 1] = 255;
+                    bits[i + 2] = 255;
+                }
+                bits[i + 3] = 255;
+            }
+            PixelFormat format =  PixelFormats.Pbgra32;
+            int bytesPerPixel = (format.BitsPerPixel + 7) / 8;
+            BitmapSource bitmap = BitmapSource.Create(width, height, 96, 96, format, null, bits, stride);
+            BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+            using (var filestream = new FileStream("output.bmp", FileMode.Create))
+            {
+                encoder.Save(filestream);
+            }
         }
     }
 }
--- GameOfLife/MainWindow.xaml.cs
+++ GameOfLife/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace GameOfLife
 {
@@ -17,10 +19,8 @@ namespace GameOfLife
             _lifeData = new LifeData();
 
         }
-
-        static Button[,] dots = new Button[20, 20];
-        static Brush[,] dotsNext = new Brush[20, 20];
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private static Button[,] dots = new Button[20, 20];
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
             for (int i = 0; i < 20; i++)
             {
@@ -28,8 +28,8 @@ namespace GameOfLife
                 {
                     Button buttons = new Button();
                     buttons.Width = 10;
-                    buttons.Uid =  $"{j},{i}";
                     buttons.Height = 10;
+                    buttons.Uid =  $"{i},{j}";
                     buttons.Click += buttons_Click;
                     buttons.Background = Brushes.White;
                     dots[i, j] = buttons;
@@ -40,110 +40,20 @@ namespace GameOfLife
         private void buttons_Click(object sender, RoutedEventArgs e)
         {
             Button thisButton = (Button)sender;
-            //TODO: remove
-            thisButton.Background = thisButton.Background == Brushes.Black ? Brushes.White : Brushes.Black;
-            int posX = int.Parse(thisButton.Uid.Split(',').First());
-            int posY = int.Parse(thisButton.Uid.Split(',').Last());
-            dots[posY, posX].Background = thisButton.Background;
-
-            _lifeData.ReverseState(posX, posY);
-            _lifeData.PaintButtons(dots);
+            int posY = int.Parse(thisButton.Uid.Split(',').First());
+            int posX = int.Parse(thisButton.Uid.Split(',').Last());
+            _lifeData.ReverseState(thisButton, posY, posX);
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            _lifeData.MakeTurn();
-            _lifeData.PaintButtons(dots);
+                _lifeData.MakeTurn();
+                _lifeData.PaintButtons(dots);
 
-            //TODO: remove
-            for (int i = 0; i < 20; i++)
-            {
-                for (int j = 0; j < 20; j++)
-                {
-                    int[] info = GetInfo(dots[i, j]);
-                    if (dots[i, j].Background == Brushes.White)
-                    {
-                        AddDots(info[0], info[1], info[2]);
-                    }
-                    else
-                    {
-                        DeleteDots(info[0], info[1], info[2]);
-                    }
-                }
-            }
-            Refreshing();
         }
-
-        private int[] GetInfo(Button item)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            int aliveCount = 0;
-            int posX = int.Parse(item.Uid.Split(',').First());
-            int posY = int.Parse(item.Uid.Split(',').Last());
-            for (int i = posY - 1; i < posY + 2; i++)
-            {
-                for (int j = posX - 1; j < posX + 2; j++)
-                {
-                    int posXCopy = j == -1 ? 19 : j == 20 ? 0 : j;
-                    int posYCopy = i == -1 ? 19 : i == 20 ? 0 : i;
-                    if (dots[posYCopy, posXCopy].Background == Brushes.Black)
-                        aliveCount++;
-                }
-            }
-            int[] info = new int[3];
-            info[0] = posX;
-            info[1] = posY;
-            info[2] = aliveCount;
-            return info;
-        }
-        private void AddDots(int x, int y, int aliveCount)
-        {
-
-            if (aliveCount == 3)
-            {
-                dotsNext[y, x] = Brushes.Black;
-            }
-            else
-            {
-                dotsNext[y, x] = Brushes.White;
-            }
-        }
-        private void DeleteDots(int x, int y, int aliveCount)
-        {
-            if (aliveCount > 3 || aliveCount < 2)
-            {
-                dotsNext[y, x] = Brushes.White;
-            }
-            else
-            {
-                dotsNext[y, x] = Brushes.Black;
-            }
-        }
-        private void Refreshing()
-        {
-            for (int i = 0; i < 20; i++)
-            {
-                for (int j = 0; j < 20; j++)
-                {
-                    dots[i, j].Background = dotsNext[i, j];
-                }
-            }
-        }
-
-        //private void ClearField()
-        //{
-        //    for (int i = 0; i < 20; i++)
-        //    {
-        //       for (int j = 0; j < 20; j++)
-        //        {
-        //            dots[i, j].Background = Brushes.White;
-        //        }
-        //    }
-        //    canChangeAfterAdd.Clear();
-        //   
-        //}
-        private void ClearButton_Click(object sender, RoutedEventArgs e)
-        {
-           // ClearField();
+            _lifeData.SaveInBMP();        
         }
     }
 }
